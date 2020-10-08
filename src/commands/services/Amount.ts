aeb3f4f27173a7f01a9ecd93789bfcc24ab7a354
sandervspl <sandervispoel@hotmail.com>
2020-10-09 00:22:32 +0200

    Add channels and detect numbers

--- src/commands/services/Amount.ts
+++ src/commands/services/Amount.ts
@@ -3,8 +3,20 @@ import Command from '../Command';
 
 export class Amount extends Command {
   constructor(discordClient: Discord.Client) {
-    super(discordClient, (msg) => {
-      return !isNaN(Number(msg))
+    super(discordClient, (msg, trimmedMsg) => {
+      const isNumberMsg = !isNaN(Number(trimmedMsg));
+
+      if (!isNumberMsg) {
+        msg.delete().then(() => {
+          msg.author.createDM().then((dmChannel) => {
+            dmChannel.send('Your message was removed because you can only send the amount of boosts in this channel.');
+          });
+        });
+      }
+
+      return isNumberMsg;
+    }, {
+      channels: ['763723790407696454', '763749549918912575'],
     });
 
     this.onCommand((msg) => {
