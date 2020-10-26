5a2a9e8ea86343983762694997801ebc1554b051
sandervspl <sandervispoel@hotmail.com>
2020-10-27 00:09:13 +0100

    Remove reset command

--- src/commands/services/index.ts
+++ src/commands/services/index.ts
@@ -1,11 +1,9 @@
 import { Commands } from './Commands';
 import { Amount } from './Amount';
 import { Clear } from './Clear';
-// import { Reset } from './Reset';
 
 export default [
   Commands,
   Amount,
   Clear,
-  // Reset,
 ] as const;
