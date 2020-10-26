4c32c63021efbe52562fe7802dc756d5419b830d
sandervspl <sandervispoel@hotmail.com>
2020-10-26 23:18:10 +0100

    Add dev env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import DiscordBot from 'DiscordBot';
+import env from 'helpers/env';
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(process.cwd(), env.isDevelopment ? '.dev.env' : '.env'),
+});
 
 const app = express();
 const PORT_DEFAULT = 5000;
